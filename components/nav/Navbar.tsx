57fbf8117379e88778911c74406892a6a95bfd16
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-12-17 23:13:33 +0530

    Navbar : without functionality

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -1,16 +1,23 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import { SiGithub } from "react-icons/si";
 
 const Navbar = () => {
   return (
-    <nav className='flex'>
-        <div className='group'>
+    <nav className="flex items-center justify-between">
+      <div className="group">
+        <Link href="/" className="font-bold text-[2rem]">
+          <span className="font-semibold">GoDevs</span>
+        </Link>
+        <div className="h-1 w-0 duration-150 group-hover:w-full transition-all bg-blue-500" />
+      </div>
 
-        <Link href="/">GoDevs</Link>
-        <div className='h-1 w-0 duration-150 group-hover:w-full transition-all bg-green-500'></div>
-        </div>
+      <Button variant="outline" className="flex items-center gap-2">
+        <SiGithub /> Login
+      </Button>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
